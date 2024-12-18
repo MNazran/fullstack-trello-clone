2684ba6957fe50522a665f5d7fa9f6908674b5d8
MNazran <muhammadnazran78@gmail.com>
2024-12-18 21:00:17 +0800

    Add sortable functionality to board columns and update package.json dependencies

--- apps/web/src/components/Board.tsx
+++ apps/web/src/components/Board.tsx
@@ -1,17 +1,44 @@
+'use client'
+
+import { useState } from 'react'
 import Column from './Column'
 import NewColumnForm from './forms/NewColumnForm'
 
-const columns = [
-  { id: 'adks', name: 'To Do', index: 0 },
-  { id: 'dfsc', name: 'In Progress', index: 1 },
-  { id: 'ioss', name: 'Done', index: 2 },
+const defaultColumns = [
+  { id: 'col1', name: 'To Do', index: 0 },
+  { id: 'col2', name: 'In Progress', index: 1 },
+  { id: 'col3', name: 'Done', index: 2 },
+]
+
+export type CardType = {
+  id: string | number
+  name: string
+  index: number
+  columnId: string
+}
+
+const defaultCards = [
+  { id: 'asdf', name: 'task 1', index: 0, columnId: 'col1' },
+  { id: 'cdns', name: 'task 2', index: 1, columnId: 'col2' },
+  { id: 'tcsg', name: 'task 3', index: 2, columnId: 'col3' },
+  { id: 'nfdc', name: 'task 4', index: 0, columnId: 'col1' },
 ]
 
 export default function Board() {
+  const [cards, setCards] = useState(defaultCards)
+  const [columns, setColumns] = useState(defaultColumns)
+
   return (
     <div className="flex gap-4">
       {columns.map((column) => (
-        <Column key={column.id} {...column} />
+        <Column
+          key={column.id}
+          {...column}
+          setCards={setCards}
+          cards={cards
+            .sort((a, b) => a.index - b.index)
+            .filter((c) => c.columnId === column.id)}
+        />
       ))}
       <NewColumnForm />
     </div>
